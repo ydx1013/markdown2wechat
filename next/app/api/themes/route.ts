5f3331bf96dabfa3802d61e933faa4a1cc4fe171
koala95271 <koala9527@git.com>
2025-12-23 17:15:06 +0800

    fix:next build

--- next/app/api/themes/route.ts
+++ next/app/api/themes/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getDefaultThemeName, listThemeNames } from "../../../lib/theme";
+import { getDefaultThemeName, listThemeNames } from "@/lib/theme";
 
 export async function GET() {
   try {
